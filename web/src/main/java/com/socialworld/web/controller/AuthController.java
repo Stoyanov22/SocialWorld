2ecc17c70880c0a1c59cf16c0c04bf0b5decfe78
Stoyanov22 <iliyan.stoyanov.22@gmail.com>
2020-03-03 13:57:09 +0200

    Catching the login form from the controller

--- web/src/main/java/com/socialworld/web/controller/AuthController.java
+++ web/src/main/java/com/socialworld/web/controller/AuthController.java
@@ -3,13 +3,22 @@ package com.socialworld.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
 
     @RequestMapping(value= {"/login"}, method= RequestMethod.GET)
-    public ModelAndView home() {
+    public ModelAndView login() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("auth/login");
+        return model;
+    }
+
+    @RequestMapping(value= {"/login"}, method= RequestMethod.POST)
+    public ModelAndView login(@RequestParam (value="email") String email, @RequestParam(value="password") String password) {
+
         ModelAndView model = new ModelAndView();
         model.setViewName("auth/login");
         return model;
