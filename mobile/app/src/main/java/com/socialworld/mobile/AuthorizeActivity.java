3e6f862c3c47d6323aec5117fdeca720ec6fd660
atanas-katsarov <atanaskatsarov1401681073@gmail.com>
2020-03-22 16:17:22 +0200

    Bug Tracker updated

--- mobile/app/src/main/java/com/socialworld/mobile/AuthorizeActivity.java
+++ mobile/app/src/main/java/com/socialworld/mobile/AuthorizeActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
