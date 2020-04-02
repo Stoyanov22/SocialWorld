0222ef9faed23dbba353cbac1e20a51d5f89c475
atanas-katsarov <atanaskatsarov1401681073@gmail.com>
2020-04-02 20:39:38 +0300

    UserEntity updated

--- mobile/app/src/main/java/com/socialworld/mobile/entities/UserEntity.java
+++ mobile/app/src/main/java/com/socialworld/mobile/entities/UserEntity.java
@@ -1,13 +1,20 @@
 package com.socialworld.mobile.entities;
 
+import com.google.firebase.database.annotations.NotNull;
+
 import java.io.Serializable;
 import java.util.Objects;
 
 public class UserEntity implements Serializable {
     private String id;
     private String email;
+    private String name;
+    private String picture;
+    private String dateOfBirth;
+    private Integer genderId;
+    private Integer countryId;
 
-    public UserEntity(String id, String email) {
+    public UserEntity(@NotNull String id, @NotNull String email) {
         this.id = id;
         this.email = email;
     }
@@ -28,25 +35,75 @@ public class UserEntity implements Serializable {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getGenderId() {
+        return genderId;
+    }
+
+    public void setGenderId(Integer genderId) {
+        this.genderId = genderId;
+    }
+
+    public Integer getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Integer countryId) {
+        this.countryId = countryId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserEntity that = (UserEntity) o;
-        return id == that.id &&
-                Objects.equals(email, that.email);
+        return id.equals(that.id) &&
+                email.equals(that.email) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(picture, that.picture) &&
+                Objects.equals(dateOfBirth, that.dateOfBirth) &&
+                Objects.equals(genderId, that.genderId) &&
+                Objects.equals(countryId, that.countryId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, email);
+        return Objects.hash(id, email, name, picture, dateOfBirth, genderId, countryId);
     }
 
     @Override
     public String toString() {
         return "UserEntity{" +
-                "id=" + id +
+                "id='" + id + '\'' +
                 ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", picture='" + picture + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", genderId=" + genderId +
+                ", countryId=" + countryId +
                 '}';
     }
 }
