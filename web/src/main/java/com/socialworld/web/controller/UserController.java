1b27a0fd3db628fb35ca0487782afb63d9577694
Stoyanov22 <iliyan.stoyanov.22@gmail.com>
2020-04-25 14:03:03 +0300

    Succesfully removing user from Auth and Firestore

--- web/src/main/java/com/socialworld/web/controller/UserController.java
+++ web/src/main/java/com/socialworld/web/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.socialworld.web.controller;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.socialworld.web.entity.CountryConstants;
 import com.socialworld.web.entity.GenderConstants;
 import com.socialworld.web.entity.User;
@@ -23,6 +25,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @RequestMapping(value = {"/my_profile"}, method = RequestMethod.GET)
     public ModelAndView myProfile(HttpSession session) {
@@ -79,6 +82,20 @@ public class UserController {
         User user = new User(session.getAttribute("uid").toString(), session.getAttribute("email").toString(), name.toString(), dob, Integer.parseInt(genderId), Integer.parseInt(countryId), pictureUrl);
         userService.editUser(user);
         model.setViewName("redirect:/my_profile");
+        return model;
+    }
+
+    @RequestMapping(value = {"/remove_user"}, method = RequestMethod.POST)
+    public ModelAndView removeUser(HttpSession session, @RequestParam String uid) {
+        try {
+            firebaseAuth.deleteUser(uid);
+            userService.removeUserById(uid);
+            session.invalidate();
+        } catch (FirebaseAuthException e) {
+            System.err.println("Couldn't remove user : " + uid);
+        }
+        ModelAndView model = new ModelAndView();
+        model.setViewName("redirect:/index");
         return model;
     }
 }
