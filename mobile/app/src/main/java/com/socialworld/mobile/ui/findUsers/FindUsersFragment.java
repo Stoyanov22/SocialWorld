9519792b6c0efff5f3430cde21de358d66605fa5
atanas-katsarov <atanaskatsarov1401681073@gmail.com>
2020-09-26 22:54:04 +0300

    Changed the used adapter and logic

--- mobile/app/src/main/java/com/socialworld/mobile/ui/findUsers/FindUsersFragment.java
+++ mobile/app/src/main/java/com/socialworld/mobile/ui/findUsers/FindUsersFragment.java
@@ -1,48 +1,57 @@
 package com.socialworld.mobile.ui.findUsers;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.paging.PagedList;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
-import com.firebase.ui.firestore.paging.FirestorePagingOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.socialworld.mobile.R;
+import com.socialworld.mobile.adapters.UsersAdapter;
 import com.socialworld.mobile.entities.UserEntity;
-import com.socialworld.mobile.models.GlideApp;
+import com.socialworld.mobile.models.FollowedUsersViewModel;
+import com.socialworld.mobile.ui.myProfile.MyProfileFragment.OnMyProfileInteractionListener;
+import com.socialworld.mobile.ui.myProfile.MyProfileViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Atanas Katsarov
  */
 public class FindUsersFragment extends Fragment {
-
+    private MyProfileViewModel myProfileViewModel;
+    private FollowedUsersViewModel followedUsersViewModel;
+    private OnMyProfileInteractionListener mListener;
     private RecyclerView usersRecView;
-    private EditText searchEditText;
-    private Query query;
-    private FirestorePagingOptions<UserEntity> options;
-    private FirestorePagingAdapter<UserEntity, FoundUsersViewHolder> adapter;
+    private SearchView searchView;
+    private UsersAdapter adapter;
     private FirebaseFirestore db;
+    private RadioGroup filterRadioGroup;
+    private RadioButton followedRadioBtn;
+    private RadioButton unfollowedRadioBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        myProfileViewModel = new ViewModelProvider(requireActivity()).get(MyProfileViewModel.class);
+        followedUsersViewModel = new ViewModelProvider(requireActivity()).get(FollowedUsersViewModel.class);
         db = FirebaseFirestore.getInstance();
     }
 
@@ -51,83 +60,83 @@ public class FindUsersFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find_users, container, false);
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext());
+        filterRadioGroup = view.findViewById(R.id.filter_radio_group);
+        followedRadioBtn = view.findViewById(R.id.radio_followed);
+        unfollowedRadioBtn = view.findViewById(R.id.radio_unfollowed);
+        searchView = view.findViewById(R.id.find_users_search);
+        searchView.setVisibility(View.INVISIBLE);
+        filterRadioGroup.setVisibility(View.INVISIBLE);
 
         usersRecView = view.findViewById(R.id.find_users_rec_view);
         usersRecView.setHasFixedSize(true);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext());
         usersRecView.setLayoutManager(layoutManager);
 
-        final PagedList.Config config = new PagedList.Config.Builder()
-                .setInitialLoadSizeHint(20)
-                .setPageSize(10)
-                .build();
+        db.collection("Users").whereEqualTo("enabled", true).get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<UserEntity> followedUsers = new ArrayList<>(followedUsersViewModel.getFollowedUsers().getUserSet());
+                        List<UserEntity> unfollowedUsers = queryDocumentSnapshots.toObjects(UserEntity.class);
+                        unfollowedUsers.remove(myProfileViewModel.getUser());
+                        unfollowedUsers.removeAll(followedUsers);
+                        adapter = new UsersAdapter(unfollowedUsers, followedUsers, mListener);
+                        usersRecView.setAdapter(adapter);
+                        usersRecView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
 
-        query = db.collection("Users");
-        options = new FirestorePagingOptions.Builder<UserEntity>()
-                .setLifecycleOwner(getViewLifecycleOwner())
-                .setQuery(query, config, UserEntity.class)
-                .build();
+                        searchView.setVisibility(View.VISIBLE);
+                        filterRadioGroup.setVisibility(View.VISIBLE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("USERS_LOG", "Couldn't load users: " + e.getMessage());
+                    }
+                });
 
-        adapter = new FirestorePagingAdapter<UserEntity, FoundUsersViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull FoundUsersViewHolder holder, int position, @NonNull UserEntity model) {
-                holder.name.setText(model.getName());
-                if (model.getPicture() != null) {
-                    GlideApp
-                            .with(holder.image.getContext())
-                            .load(model.getPicture())
-                            .circleCrop()
-                            .into(holder.image);
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        return false;
+                    }
 
-                } else {
-                    GlideApp
-                            .with(holder.image.getContext())
-                            .clear(holder.image);
-                }
-            }
-
-            @NonNull
-            @Override
-            public FoundUsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item_user, parent, false);
-                return new FoundUsersViewHolder(view);
-            }
-        };
-        usersRecView.setAdapter(adapter);
-        usersRecView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
-
-        searchEditText = view.findViewById(R.id.find_users_search);
-        ImageButton searchBtn = view.findViewById(R.id.find_users_btn);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (searchEditText.getText().length() == 0) {
-                    Toast.makeText(requireContext(), R.string.please_input_username, Toast.LENGTH_LONG).show();
-                    return;
-                }
-                query = db.collection("Users").whereEqualTo("name", searchEditText.getText().toString());
-                options = new FirestorePagingOptions.Builder<UserEntity>()
-                        .setLifecycleOwner(getViewLifecycleOwner())
-                        .setQuery(query, config, UserEntity.class)
-                        .build();
-                adapter.updateOptions(options);
-            }
-        });
-
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        refreshOptionsInAdapter(newText);
+                        return false;
+                    }
+                });
+                View.OnClickListener radioButtonClickedListener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        refreshOptionsInAdapter(searchView.getQuery().toString());
+                    }
+                };
+                followedRadioBtn.setOnClickListener(radioButtonClickedListener);
+                unfollowedRadioBtn.setOnClickListener(radioButtonClickedListener);
         return view;
     }
 
-    private class FoundUsersViewHolder extends RecyclerView.ViewHolder {
-        private ImageView image;
-        private TextView name;
-        private Button followBtn;
+    private void refreshOptionsInAdapter(String searchString) {
+        boolean isLookingForFollowed = filterRadioGroup.getCheckedRadioButtonId() == followedRadioBtn.getId();
+        adapter.filterOptionsByString(searchString, isLookingForFollowed);
+    }
 
-        public FoundUsersViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            image = itemView.findViewById(R.id.user_item_img);
-            name = itemView.findViewById(R.id.user_item_name);
-            followBtn = itemView.findViewById(R.id.user_item_follow_btn);
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnMyProfileInteractionListener) {
+            mListener = (OnMyProfileInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnMyProfileInteractionListener");
         }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
     }
 }
